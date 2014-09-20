4d68bcbbd70ad3072344cac5eec843d4528964ce
Yuichi Murata <mrk21info+github@gmail.com>
2014-09-20 23:09:21 +0900

    Made it possible to build ui files.

--- src/twit4v/main_window.cpp
+++ src/twit4v/main_window.cpp
@@ -1,7 +1,14 @@
 #include <twit4v/main_window.hpp>
+#include "ui_main_window.h"
 
 namespace Twit4v {
-    MainWindow::MainWindow(QWidget * parent) : QMainWindow(parent), label("hello world") {
-        this->setCentralWidget(&this->label);
+    MainWindow::MainWindow(QWidget * parent) : QMainWindow(parent), ui(std::make_shared<Ui::MainWindow>()) {
+        this->ui->setupUi(this);
+    }
+    
+    void MainWindow::on_submitButton_clicked() {
+        QString string = this->ui->lineEdit->text();
+        this->ui->lineEdit->clear();
+        this->ui->textEdit->append(string);
     }
 }
--- twit4v/main_window.hpp
+++ twit4v/main_window.hpp
@@ -2,17 +2,24 @@
 #define TWIT4V_MAIN_WINDOW_HPP
 
 #include <QMainWindow>
-#include <QLabel>
+#include <memory>
 
 namespace Twit4v {
+    namespace Ui {
+        class MainWindow;
+    }
+    
     class MainWindow: public QMainWindow {
         Q_OBJECT
         
     public:
         explicit MainWindow(QWidget * parent = nullptr);
         
+    private slots:
+        void on_submitButton_clicked();
+        
     private:
-        QLabel label;
+        std::shared_ptr<Ui::MainWindow> ui;
     };
 }
 
